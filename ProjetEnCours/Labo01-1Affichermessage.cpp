0cfec517f9762361b9154bddf3fb17e44af8cc65
francis breault <70411383+fbreault-cstj@users.noreply.github.com>
2021-08-26 13:27:47 -0400

    Add files via upload

---
+++ ProjetEnCours/Labo01-1Affichermessage.cpp
@@ -0,0 +1,18 @@
+//But: Écrire un programme qui affiche un message à l'écran dans la console
+//Nom:Francis Breault
+//Date:2021-08-26
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+
+
+
+
+
+	return 0;
+}
