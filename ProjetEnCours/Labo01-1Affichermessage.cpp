1d54f6408a206d57d9c4c8d60eb156a21e670b0c
francis breault <70411383+fbreault-cstj@users.noreply.github.com>
2021-08-26 13:50:37 -0400

    Update Labo01-1Affichermessage.cpp

--- ProjetEnCours/Labo01-1Affichermessage.cpp
+++ ProjetEnCours/Labo01-1Affichermessage.cpp
@@ -1,4 +1,4 @@
-//But: �crire un programme qui affiche un message � l'�cran dans la console
+//But: Écrire un programme qui affiche un message à l'écran dans la console
 //Nom:Francis Breault
 //Date:2021-08-26
 
@@ -6,13 +6,24 @@
 
 using namespace std;
 
-int main()
+
+// Tout programme doit commencer par la fonction principale qui s'Appele main
+// toute fonction en informatique sonjt suivie de paranthèse pour dresser la liste des paramètres
+// Si la liste de paramètres est vide,les parentheses sont obligatoire et le contenu reste vide ()
+// une fonction peut ou pas retourner un résultat. 
+//On doit indiquer le type de résultat attendu avant le nom de la fonction. ici on ne retourne rien donc VOID
+void main()
+
+// les instructions doivent être placées dans un bloc d'instruction qui en C++ s'ouvre avec des accolades {}
+// l'accolade ouvrante doit être aligné verticalement avec l'acolade fermante
+// les instruction doivent etre décalé d'une tabulation pour montrer qu'elles sont a l'intérieur du bloc
+// on indente le code pour améliorer sa lisibilité
 {
 
 
+	// Afficher un message à l'écran
+	cout << "CYKABLYAT" << endl;
 
 
-
-
-	return 0;
 }
+
